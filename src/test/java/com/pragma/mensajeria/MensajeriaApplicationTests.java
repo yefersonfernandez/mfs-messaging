2f8ecc19ed7e20b8945ba0a2687503594c116fba
yafernandez <yeferson1500@gmail.com>
2025-03-01 18:32:45 -0500

    chore: initial commit from skeleton

---
+++ src/test/java/com/pragma/mensajeria/MensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.mensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
